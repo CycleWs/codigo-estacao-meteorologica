0e58d68c4b1be758ff8b9829f12cefa274509f6f
CycleWs <joaohlspj@gmail.com>
2023-11-05 01:06:57 -0300

    first commit

---
+++ codigoEstacaoMeteorologica.ino
@@ -0,0 +1,215 @@
+#ifdef ESP8266
+ #include <ESP8266WiFi.h>
+ #else
+ #include <WiFi.h>
+#endif
+
+#include "DHTesp.h"
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+
+/**** DHT11 sensor Settings *******/
+#define DHTpin 5   //Set DHT pin as GPIO2
+DHTesp dht;
+
+/****** WiFi Connection Details *******/
+const char* ssid = "Sua-Rede";
+const char* password = "Sua-Senha";
+
+/******* MQTT Broker Connection Details *******/
+const char* mqtt_server = "broker.hivemq.com";
+const char* mqtt_username = "SeuUsernameMQTT";
+const char* mqtt_password = "SuaSenhaMQTT";
+const int mqtt_port =8883;
+
+const int sensorPin = 4;
+volatile unsigned long count = 0;
+unsigned long startTime = 0;
+float circunference = 0.10;
+float kmh = 0;
+float rainPercentage = 0;
+
+
+void ICACHE_RAM_ATTR countRotation();
+
+void ICACHE_RAM_ATTR countRotation() {
+  // Incrementa a contagem sempre que o sensor é ativado
+  count++;
+}
+
+/**** Secure WiFi Connectivity Initialisation *****/
+WiFiClientSecure espClient;
+
+/**** MQTT Client Initialisation Using WiFi Connection *****/
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+
+static const char *root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
+void setup_wifi() {
+  delay(10);
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("\nWiFi connected\nIP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";   // Create a random client ID
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+
+      client.subscribe("Teste");   // subscribe the topics here
+
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");   // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String incommingMessage = "";
+  for (int i = 0; i < length; i++) incommingMessage+=(char)payload[i];
+
+  Serial.println("Message arrived ["+String(topic)+"]"+incommingMessage);
+
+  //--- check the incomming message
+
+}
+
+void publishMessage(const char* topic, String payload , boolean retained){
+  if (client.publish(topic, payload.c_str(), true))
+      Serial.println("Message publised ["+String(topic)+"]: "+payload);
+}
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(sensorPin), countRotation, RISING);
+  dht.setup(DHTpin, DHTesp::DHT11); //Set up DHT11 sensor
+  while (!Serial) delay(1);
+  setup_wifi();
+
+  #ifdef ESP8266
+    espClient.setInsecure();
+  #else
+    espClient.setCACert(root_ca);      // enable this line and the the "certificate" code for secure connection
+  #endif
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+/******** Main Function *************/
+void loop() {
+
+  if (!client.connected()) reconnect(); // check if client is connected
+  client.loop();
+
+  if(millis() - startTime >= 1000) {
+    // A cada segundo, calcule o RPM e imprima
+    float rpm = (count / 2.0) * 60.0;  // Divida por 2 para considerar apenas metade das marcações
+    float linearVelocity = (rpm * circunference) / 60.0;
+    kmh = linearVelocity * 3.6;  // Conversão para KM/H
+    Serial.print("KM/h: ");
+    Serial.println(kmh);
+    count = 0;  // Redefina a contagem
+    startTime = millis();
+  }  
+
+
+//read DHT11 temperature and humidity reading
+  delay(dht.getMinimumSamplingPeriod());
+  float h = dht.getHumidity();
+  float t = dht.getTemperature();
+  rainPercentage =  ((h/70)/(100-70));
+  // testa se retorno é valido, caso contrário algo está errado.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Failed to read from DHT");
+  } 
+  else
+  {
+
+    // update Serial with lnall of the above graphics
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.println("%");
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println("*C");
+    Serial.print("Chance de chuva: ");
+    Serial.print(rainPercentage);
+    Serial.println("%");
+  }
+  
+
+  DynamicJsonDocument doc(1024);
+
+  doc["humity"] = h;
+  doc["temperature"] = t;
+  doc["windspeed"] = kmh;
+  doc["rainperc"] = rainPercentage;
+
+  char mqtt_message[128];
+  serializeJson(doc, mqtt_message);
+
+  publishMessage("Estação Meteorologica", mqtt_message, true);
+
+  delay(5000);
+
+}
